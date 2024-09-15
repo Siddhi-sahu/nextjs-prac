caf408261038f3e33e46dfe72c1a69875fc0473b
Siddhi Sahu <himanisahu739@gmail.com>
2024-09-15 18:43:21 +0530

    server side and databases learned

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,20 +1,33 @@
+import { NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient()
+
+export async function POST(req: NextRequest) {
+    //extract the body
+
+    const body = await req.json()
+
+    //store the body in t he database
+
+    await client.user.create({
+        data: {
+            username: body.username,
+            password: body.password
+        }
+    })
+
+    //send response to user
+
+    return Response.json({
+        message: "You are logged in"
+    })
+
+}
+
 export function GET() {
     return Response.json({
-        email: "himanisahu@gmail.com",
-        name: "himani"
-    })
-}
-
-
-export function POST() {
-    return Response.json({
-        email: "himani@gmail.com",
-        name: "himani"
-    })
-}
-export function PUT() {
-    return Response.json({
-        email: "himani@gmail.com",
-        name: "himani"
+        name: "himani",
+        email: "himanin@gmail.com"
     })
 }
